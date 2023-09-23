24f9e2ddba2c4cf49f3c9d86fd3e4d347102bc96
Sudarshan Sumathipala <118509143+sumathipala-hes@users.noreply.github.com>
2023-09-23 13:44:08 +0530

    form completed

--- application/routes.py
+++ application/routes.py
@@ -1,6 +1,13 @@
 from application import app
 from flask import render_template
+# from forms import TodoForm 
+from .forms import TodoForm
 
 @app.route("/")
 def index():
     return render_template("view_todos.html", title = "Todos")
+
+@app.route("/add_todo")
+def add_todo():
+    form = TodoForm()
+    return render_template("add_todo.html", form = form)
